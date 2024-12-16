9d2ca4b1db4b8d717faa54832e98422a4210b0a7
as2811-project <anandh.tedxchennai@gmail.com>
2024-12-16 17:10:57 +1100

    deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
